0498643faf1c83eb2e2adc8bb9bc5ad562e2c2d2
Wight554 <wight554@gmail.com>
2018-04-23 16:47:07 +0000

    audio: Nuke audio amplifier support
    * Xiaomi are shipping their code in kernel
    
    Revert "audio: add amplifier hooks for stream parameter manipulation"
    
    This reverts commit 56eeef325ea842144e53fd09eb41af3debdd992e.
    
    Revert "hal: switch audio_extn_set_parameters and amplifier_set_parameters"
    
    This reverts commit 33c6c64dc8c592025d2aaaa11e25d944bd3fa065.
    
    Revert "hal: Support the audio amplifier hook"
    
    This reverts commit f19eb03b30878589973498f9274d93dc339210fc.

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -40,7 +40,6 @@
 
 #include <stdlib.h>
 #include <cutils/list.h>
-#include <hardware/audio_amplifier.h>
 #include <hardware/audio.h>
 #include <tinyalsa/asoundlib.h>
 #include <tinycompress/tinycompress.h>
@@ -504,7 +503,6 @@ struct audio_device {
     bool bt_sco_on;
     struct audio_device_config_param *device_cfg_params;
     unsigned int interactive_usecase_state;
-    amplifier_device_t *amp;
 };
 
 int select_devices(struct audio_device *adev,
